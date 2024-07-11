7f0a0515e1dd14a53d5353edb9fd4a6fb618dfb3
Maria <maria.g1611@yahoo.com>
2024-07-11 01:46:43 +0100

    stuff

---
+++ index.js
@@ -0,0 +1,78 @@
+//console.log("Hello World")
+
+//1)basic hello function
+// function greetSomeone(name) {
+//     return "Hello " + name;
+// }
+
+// console.log (greetSomeone("eric"));
+
+//2)attempt with backticks and $
+//  function greetSomeone(name) {
+//      return `Hello ${name}`;
+//  }
+
+//  console.log (greetSomeone("eric"));
+
+//let myAge = 31
+//let myName = "Maria"
+
+//console.log (myAge)
+//shows in console log as 31 when myAge inputed 
+
+// let myAge = 31
+// myAge = myAge + 1
+// console.log (myAge)
+//shows in console log as 32 
+
+//LOGIC FLOW/LOOPS-!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// let myAge = 101
+// while(myAge >= 101) {
+// alert ("Happy Birthday");
+// myAge++;
+// }
+
+// console.log("Happy Birthday")
+//lol this breaks the computer hhhhh
+
+//loop attempt (originally in place of pancakes was i)
+// let pancakes = 0;
+// while (pancakes < 3) { // shows 0, then 1, then 2
+//   alert( pancakes );
+//   pancakes++;
+// }
+// console.log (pancakes)
+
+//CONDITIONAL STATEMENTS
+// let hour =0
+// if (hour < 18) {
+//     greeting = "Good day";
+//   } 
+
+//   console.log("Good day")
+
+// let money = 0
+// if (money > 500) {
+//     alert ("Yay you progress to the next level");
+    
+// }
+//     else 
+//     {alert("Oh no you cannot pass this level. Go back and collect more money")
+        
+//     }
+
+//arrays arrays arrays arrays arrays arrays arrays arrays arrays arrays arrays arrays arrays
+//below method is not array its just a generic list
+// const foods = {
+//     1:"Apples",
+//     2:"Bananas",
+//    3:"Watermelon",
+// }
+// foods [2];
+
+// console.log(foods[2])
+
+const foodsArray = ["apples", "pears", "peaches", "cauliflower"]
+foodsArray.push ("oranges")
+console.log(foodsArray[4])
